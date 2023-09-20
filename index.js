b393da9a154992e501c177999949941006594da1
PrasanthChandaka <prasanthchandaka111a@gmail.com>
2023-09-21 03:15:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const flip=document.getElementById("flip");
+const hover=document.getElementById("hover");
+const flex=document.getElementById("flex");
+
+function action(){
+    hover.classList.toggle("contain1")
+    flex.classList.toggle("flex")
+}
